c324ff397e7e0f6615713ad9831d1d274a9da575
Edigar de Almeida Carvalho <106999716+EdigarCarvalho@users.noreply.github.com>
2023-11-20 00:13:10 -0300

    feat: Player2
    In addition to adding player 2 movement and details, I tried to make some improvements to the game's performance.

--- src/main/java/com/mycompany/game/Game.java
+++ src/main/java/com/mycompany/game/Game.java
@@ -13,20 +13,22 @@ import java.io.IOException;
 import java.net.URL;
 import javax.imageio.ImageIO;
 
-public class Game extends JPanel implements ActionListener , MouseListener{
+public class Game extends JPanel implements ActionListener {
     private Timer timer;
     private int score;
     private int lives;
     private int playerX;
     private int playerY;
+    private int player2X;
+    private int player2Y;
     private int enemyX;
     private int enemyY;
     private int enemyXSpeed;
     private int enemyYSpeed;
-    private int lar; // Largura da tela
-    private int alt; // Altura da tela
-    private BufferedImage [] playerImage = new BufferedImage[8]; 
-    private BufferedImage [] enemyImage = new BufferedImage[2]; 
+    private int lar; 
+    private int alt; 
+    private BufferedImage[] playerImage = new BufferedImage[8]; 
+    private BufferedImage[] enemyImage = new BufferedImage[2]; 
     private BufferedImage gameOver;
     private int step = 1;
     private int enemyStep = 1;
@@ -38,73 +40,87 @@ public class Game extends JPanel implements ActionListener , MouseListener{
     private boolean pressedDown;
     private boolean pressedUp;
     private Image bird;
+
+    private SwingWorker<Void, Void> birdWorker = new SwingWorker<Void, Void>() {
+        @Override
+        protected Void doInBackground() throws Exception {
+            while (true) {
+                repaint();
+                Thread.sleep(99999999); 
+                
+            }
+        }
+    };
+
     public Game() {
         Dimension tela = Toolkit.getDefaultToolkit().getScreenSize();
         lar = (int) tela.getWidth();
         alt = (int) tela.getHeight();
-        timer = new Timer(10, this);
+        timer = new Timer(11, this);
         score = 0;
         lives = 2;
         playerX = lar / 2 - 25;
         playerY = alt / 2 - 25; 
+        player2X = playerX;
+        player2Y = playerY;
         respawnEnemy();
 
         setFocusable(true);
         addKeyListener(new MyKeyListener());
         setPlayerSprites();
         setEnemySprites();
-        
+
         try {
             backgroundImage = ImageIO.read(getClass().getResource("/background.jpeg"));
         } catch (IOException e) {
             System.out.println(e);
         }
-        
+
         try {
-        heartImage = ImageIO.read(getClass().getResource("/heart.png"));
+            heartImage = ImageIO.read(getClass().getResource("/heart.png"));
         } catch (IOException e) {
             System.out.println(e);
         }
-        
+
         try {
-        gameOver = ImageIO.read(getClass().getResource("/gameover.png"));
+            gameOver = ImageIO.read(getClass().getResource("/gameover.png"));
         } catch (IOException e) {
             System.out.println(e);
         }
-        
+
         try {
-            bird = new ImageIcon(new URL("https://steamuserimages-a.akamaihd.net/ugc/958586341173600765/019F0AFA8BF24C501666B04A7F1FD61D233C6E32/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false")).getImage();
+            bird = new ImageIcon(new URL("https://static.tildacdn.com/tild3966-6339-4137-a236-623866643864/bird-flap-animation.gif")).getImage();
         } catch (IOException e) {
             System.out.println(e);
         }
-        
-        
-        
+
+        addMouseMotionListener(new MyMouseMotionAdapter());
+
+        // Start the SwingWorker for bird movement
+        birdWorker.execute();
+
         timer.start();
     }
-    
+
+    private void loadImage(String path, BufferedImage[] images, int numImages) {
+        for (int i = 1; i <= numImages; i++) {
+            String valor = Integer.toString(i);
+            try {
+                images[i - 1] = ImageIO.read(getClass().getResource(path + valor + ".png"));
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+    }
+
     private void setPlayerSprites() {
-        for (int i = 1; i <= 8; i++) {
-            String valor = Integer.toString(i);
-            try {
-                playerImage[i - 1] = ImageIO.read(getClass().getResource("/ciclo" + valor + ".png"));
-            } catch (IOException e) {
-                System.out.print(e);
-            }
-        }
+        loadImage("/ciclo", playerImage, 8);
     }
-    
+
     private void setEnemySprites() {
-        for (int i = 1; i <= 2; i++) {
-            String valor = Integer.toString(i);
-            try {
-                enemyImage[i - 1] = ImageIO.read(getClass().getResource("/inimigo" + valor + ".png"));
-            } catch (IOException e) {
-                System.out.print(e);
-            }
-        }
+        loadImage("/inimigo", enemyImage, 2);
     }
-    
+
     private void respawnEnemy() {
         enemyX = (int) (Math.random() * lar);
         enemyY = (int) (Math.random() * (alt / 2)); 
@@ -114,13 +130,13 @@ public class Game extends JPanel implements ActionListener , MouseListener{
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        
+
         if (backgroundImage != null) {
             g.drawImage(backgroundImage, 0, 0, lar, alt, this);
         }
-        
+
         if (bird != null) {
-            g.drawImage(bird, 0, 0, this);
+            g.drawImage(bird, player2X, player2Y, 100, 100, this);
         }
         
          g.setColor(Color.WHITE);
@@ -194,9 +210,11 @@ public class Game extends JPanel implements ActionListener , MouseListener{
     }
 
     public boolean checkCollision() {
-        Rectangle playerBounds = new Rectangle(playerX, playerY, 50, 50);
-        Rectangle enemyBounds = new Rectangle(enemyX, enemyY, 50, 50);
-        return playerBounds.intersects(enemyBounds);
+        Rectangle playerBounds1 = new Rectangle(playerX, playerY, 50, 50);
+        Rectangle playerBounds2 = new Rectangle(player2X, player2Y, 100, 100); 
+        Rectangle enemyBounds = new Rectangle(enemyX, enemyY, 50 * enemyStep, 50);
+
+        return playerBounds1.intersects(enemyBounds) || playerBounds2.intersects(enemyBounds);
     }
     
     private void checkOutOfBounds() {
@@ -215,38 +233,34 @@ public class Game extends JPanel implements ActionListener , MouseListener{
         }
     }
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
+    private void updateBirdPosition(MouseEvent e) {
+        player2X = e.getX() - 50;
+        player2Y = e.getY() - 50;
+    }
+
+    public void mouseDragged(MouseEvent me) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-    @Override
-    public void mousePressed(MouseEvent e) {
+    public void mouseMoved(MouseEvent me) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
+    
+    private class MyMouseMotionAdapter extends MouseMotionAdapter {
     @Override
-    public void mouseReleased(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void mouseMoved(MouseEvent e) {
+        updateBirdPosition(e);
+        repaint();
     }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+}
 
 private class MyKeyListener extends KeyAdapter {
-    private double speedPercentage = 0.05; // Ajuste o valor conforme necessário
+    private double speedPercentage = 0.05; 
 
     public void keyPressed(KeyEvent e) {
         int keyCode = e.getKeyCode();
-        int playerWidth = 50; // Largura do jogador
-        int playerHeight = 50; // Altura do jogador
+        int playerWidth = 50; 
+        int playerHeight = 50; 
 
         if (keyCode == KeyEvent.VK_LEFT && playerX > 0) {
             playerX -= (int) (lar * speedPercentage);
@@ -278,7 +292,7 @@ private class MyKeyListener extends KeyAdapter {
             public void run() {
                 JFrame frame = new JFrame("O Jogo mais incrível");
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setExtendedState(JFrame.MAXIMIZED_BOTH); // Maximiza a janela
+                frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
                 Game game = new Game();
                 frame.add(game);
                 frame.setVisible(true);
